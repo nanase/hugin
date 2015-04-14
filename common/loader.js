33c8fb9a27525c3d4b802309f2aa7b7910e8a8d8
Tomona Nanase <nanase@users.noreply.github.com>
2015-04-14 15:27:42 +0900

    First commit

---
+++ common/loader.js
@@ -0,0 +1,23 @@
+root_directory = null;
+
+function loader_load(dir) { 
+  root_directory = dir;
+  var css = [
+	    'common/bootstrap.min.css',  
+	    'common/bootstrap-theme.min.css',
+	    'common/jquery-ui.min.css',
+	    'common/markdown.css',
+	    'common/index.css'
+  ];
+	var js = [
+		'common/jquery-2.1.3.min.js',  
+		'common/bootstrap.min.js',
+		'common/jquery-ui.min.js',
+		'common/jquery.markdown.js',
+		'common/index.js'
+	];
+  for (var i = 0; i < css.length; i++)
+    document.write('<link rel="stylesheet" href="' + dir + css[i] + '" />');
+	for (var i = 0; i < js.length; i++)
+		document.write('<script src="' + dir + js[i] + '"></script>');
+}
